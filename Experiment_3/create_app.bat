3385cfef4866a22472e044b0996de351eefbe087
Premdeep-Singh <premdeep1911@gmail.com>
2026-02-06 12:51:56 +0530

    Experiment 4 upload

--- Experiment_3/create_app.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-pushd "%~dp0"
-npm create vite@latest EcoTrack -- --template react
-popd
---
+++ create_app.bat
@@ -0,0 +1,5 @@
+@echo off
+
+pushd "%~dp0"
+npm create vite@latest react-memo -- --template react
+popd
